0bd9e254a658adf92c25214ae1f0d72db4cd4cb0
FaiChou <faichou.zh@gmail.com>
2021-11-13 09:51:40 +0800

    refactor

--- recursive_descent_parser.c
+++ recursive_descent_parser.c
@@ -14,22 +14,6 @@
            | Num
 */
 
-/*
-<expr> ::= <term> <expr_tail>
-
-<expr_tail> ::= + <term> <expr_tail>
-              | - <term> <expr_tail>
-              | <empty>
-
-<term> ::= <factor> <term_tail>
-
-<term_tail> ::= * <factor> <term_tail>
-              | / <factor> <term_tail>
-              | <empty>
-
-<factor> ::= ( <expr> ) |  Num
-*/
-
 enum {
   Num
 };
@@ -39,49 +23,29 @@ int token_val;
 char* line = NULL;
 char* src = NULL;
 int expr();
-void match(int);
+void next();
 
 int factor() {
-  int value = 0;
+  int v = 0;
   if (token == '(') {
-    match('(');
-    value = expr();
-    match(')');
+    next();
+    v = expr();
+    next();
   } else {
-    value = token_val;
-    match(Num);
+    v = token_val;
+    next();
   }
-  return value;
-}
-
-int term_tail(int lvalue) {
-  if (token == '*') {
-    match('*');
-    int value = lvalue * factor();
-    return term_tail(value);
-  } else if (token == '/') {
-    match('/');
-    int value = lvalue / factor();
-    return term_tail(value);
-  } else {
-    return lvalue;
-  }
+  return v;
 }
 
 int term() {
   int lvalue = factor();
-  return term_tail(lvalue);
-}
-
-int expr_tail(int lvalue) {
-  if (token == '+') {
-    match('+');
-    int value = lvalue + term();
-    return expr_tail(value);
-  } else if (token == '-') {
-    match('-');
-    int value = lvalue - term();
-    return expr_tail(value);
+  if (token == '*') {
+    next();
+    return lvalue * term();
+  } else if (token == '/') {
+    next();
+    return lvalue / term();
   } else {
     return lvalue;
   }
@@ -89,15 +53,22 @@ int expr_tail(int lvalue) {
 
 int expr() {
   int lvalue = term();
-  return expr_tail(lvalue);
+  if (token == '+') {
+    next();
+    return lvalue + expr();
+  } else if (token == '-') {
+    next();
+    return lvalue - expr();
+  } else {
+    return lvalue;
+  }
 }
 void next() {
   // skip white space
   while (*src == ' ' || *src == '\t') {
-    printf("skip white space\n");
     src++;
   }
-  printf("%c\n", *src);
+  // printf("%c\n", *src);
   token = *src++;
 
   if (token >= '0' && token <= '9') {
@@ -110,14 +81,6 @@ void next() {
     }
     return;
   }
-}
-
-void match(int tk) {
-  if (token != tk) {
-    printf("expected token: %d(%c), got: %d(%c)\n", tk, tk, token, token);
-    exit(-1);
-  }
-  next();
 }
 
 int main(int argc, char* argv[])
