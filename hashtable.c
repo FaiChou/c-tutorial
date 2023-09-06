4f0f157c2b2cc6cb7ce4fcb180c34eb87bb1e09e
周辉 <faichou.zh@gmail.com>
2023-09-06 18:35:14 +0800

    improve hash table

--- hashtable.c
+++ hashtable.c
@@ -6,6 +6,7 @@
 
 #define MAX_NAME 256
 #define TABLE_SIZE 10
+#define DELETED_NODE (person *)(0xFFFFFFFFFFFFFUL)
 
 typedef struct {
   char name[MAX_NAME];
@@ -29,6 +30,8 @@ void print_table() {
   for (int i = 0; i < TABLE_SIZE; ++i) {
     if (hash_table[i] == NULL) {
       printf("\t%i\t---\n", i);
+    } else if (hash_table[i] == DELETED_NODE) {
+      printf("\t%i\t---<deleted>\n", i);
     } else {
       printf("\t%i\t%s\n", i, hash_table[i]->name);
     }
@@ -45,32 +48,66 @@ void init_hash_table() {
 bool hash_table_insert(person *p) {
   if (p == NULL) return false;
   int index = hash(p->name);
-  // printf("%d\n", index);
-  if (hash_table[index] != NULL) return false;
-  hash_table[index] = p;
-  return true;
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    int try = (i + index) % TABLE_SIZE;
+    if (hash_table[try] == NULL || hash_table[try] == DELETED_NODE) {
+      hash_table[try] = p;
+      return true;
+    }
+  }
+  return false;
 }
 
 person *hash_table_lookup(char *name) {
   int index = hash(name);
-  if (hash_table[index] != NULL &&
-    strncmp(hash_table[index]->name, name, MAX_NAME) == 0) {
-    return hash_table[index];
-  } else {
-    return NULL;
+  for (int i = 0; i < TABLE_SIZE; ++i) {
+    int try = (index + i) % TABLE_SIZE;
+    if (hash_table[try] == NULL) return NULL;
+    if (hash_table[try] == DELETED_NODE) continue;
+    if (strncmp(hash_table[try]->name, name, MAX_NAME) == 0) {
+      return hash_table[try];
+    }
   }
+  return NULL;
+}
+
+person *hash_table_delete(char *name) {
+  int index = hash(name);
+  for (int i = 0; i < TABLE_SIZE; ++i) {
+    int try = (index + i) % TABLE_SIZE;
+    if (hash_table[try] == NULL) return NULL;
+    if (hash_table[try] == DELETED_NODE) continue;
+    if (strncmp(hash_table[try]->name, name, MAX_NAME) == 0) {
+      person *tmp = hash_table[try];
+      hash_table[try] = DELETED_NODE;
+      return tmp;
+    }
+  }
+  return NULL;
 }
 
 int main() {
   init_hash_table();
-  print_table();
   person jacob = { .name="Jacob", .age=12 };
   person kate = { .name="Kate", .age=13 };
   person mpho = { .name="Mpho", .age=1 };
+
+  person sarah = {.name= "Sarah", .age=54} ;
+  person edna = {. name="Edna", .age=15} ;
+  person maren = {. name= "Maren", .age=25};
+  person eliza = { .name="Eliza", .age=34};
+  person robert = {.name="Robert", .age=13};
+  person Jane = {.name="Jane", .age=75};
   hash_table_insert(&jacob);
   hash_table_insert(&kate);
   hash_table_insert(&mpho);
-
+  hash_table_insert(&sarah);
+  hash_table_insert(&edna);
+  hash_table_insert(&maren);
+  hash_table_insert(&eliza);
+  hash_table_insert(&robert);
+  hash_table_insert(&Jane);
+  print_table();
   person *tmp = hash_table_lookup("Mpho");
   if (tmp == NULL) {
     printf("Not found\n");
@@ -84,6 +121,13 @@ int main() {
     printf("Found %s\n", tmp->name);
   }
 
+  hash_table_delete("Mpho");
+  tmp = hash_table_lookup("Mpho");
+  if (tmp == NULL) {
+    printf("Not found\n");
+  } else {
+    printf("Found %s\n", tmp->name);
+  }
   print_table();
   return 0;
 }
