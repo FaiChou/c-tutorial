f0bd43c602f63459b638a3f21799acfca111f5fc
周辉 <faichou.zh@gmail.com>
2023-09-07 17:07:31 +0800

    b search tree

--- treeexample.c
+++ treeexample.c
@@ -51,24 +51,50 @@ void printtree(treenode *root) {
   printtree_rec(root, 0);
 }
 
+bool insertnumber(treenode **rootptr, int value) {
+  treenode *root = *rootptr;
+  if (root == NULL) {
+    // tree is empty
+    *rootptr = createnode(value);
+    return true;
+  }
+  if (value == root->value) {
+    return false;
+  }
+  if (value < root->value) {
+    return insertnumber(&(root->left), value);
+  } else {
+    return insertnumber(&(root->right), value);
+  }
+}
+
+bool findnumber(treenode *root, int value) {
+  if (root == NULL) return false;
+  if (root->value == value) {
+    return true;
+  }
+  if (value < root->value) {
+    return findnumber(root->left, value);
+  } else {
+    return findnumber(root->right, value);
+  }
+}
+
 int main() {
-  treenode *n1 = createnode(10);
-  treenode *n2 = createnode(11);
-  treenode *n3 = createnode(12);
-  treenode *n4 = createnode(13);
-  treenode *n5 = createnode(14);
+  treenode *root = NULL;
+  
+  insertnumber(&root, 15);
+  insertnumber(&root, 11);
+  insertnumber(&root, 24);
+  insertnumber(&root, 5);
+  insertnumber(&root, 19);
+  insertnumber(&root, 16);
+  printtree(root);
 
-  n1->left = n2;
-  n1->right = n3;
-  n3->left = n4;
-  n3->right = n5;
-
-  printtree(n1);
-
-  free(n1);
-  free(n2);
-  free(n3);
-  free(n4);
-  free(n5);
+  printf("%d (%d)\n", 16, findnumber(root, 16));
+  printf("%d (%d)\n", 15, findnumber(root, 15));
+  printf("%d (%d)\n", 5, findnumber(root, 5));
+  printf("%d (%d)\n", 1, findnumber(root, 1));
+  printf("%d (%d)\n", 7, findnumber(root, 7));
   return 0;
 }
